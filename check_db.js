6defd0b7f9bee7cc4530cc81e98dcb71b7ffaafb
Huangyihe888 <huangyihe@users.noreply.github.com>
2026-06-17 21:07:12 +0800

    chore: remove temp files

--- check_db.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('./backend/node_modules/@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const feedbacks = await prisma.feedback.findMany();
-  console.log(feedbacks);
-}
-main().finally(() => prisma.$disconnect());
